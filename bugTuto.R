859ebcfeccc89cf97aa065c0d4135ed6654d8c24
Edouard Hirchaud <edouard.hirchaud@univ-nantes.fr>
2013-08-26 17:11:18 +0200

    add file for debugin exercice

---
+++ bugTuto.R
@@ -0,0 +1,16 @@
+#Ligne de commande erron?e.
+matriceBug <- read.delin("Rmatrix.txt" ,  rown.names = 1  head = true)
+
+matriceBug <- read.delim("Rmatrice.txt" ,  row.names = 1 , head = TRUE)
+
+vectorSum <- function (v1, v2) {
+  v1+v2
+}
+
+#Erreur 1 : 
+#Erreur 2 : 
+#Erreur 3 : 
+#Erreur 4 :
+#Erreur 5 : 
+
+#Bonne ligne de commande :
